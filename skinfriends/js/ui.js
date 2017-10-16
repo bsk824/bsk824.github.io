0b0f954388c757a41d317cb4f0d652718b759f25
bsk824 <bsk824@gmail.com>
2017-10-16 16:54:57 +0900

    Update ui.js

--- skinfriends/js/ui.js
+++ skinfriends/js/ui.js
@@ -1,14 +1,76 @@
-$(function() {
-	if (getCookie('hello') != 'end') {
-		var hello = $('.hello');
-		hello.show();
-		setTimeout(function(){
-			hello.addClass('startHello');
-		},1);
-		setTimeout(function(){
-			hello.fadeOut();
-			setCookie('hello','end',1);
-		},8000);
+$(function(){
+	$.fn.gnb = function() {
+		var gnbWrap = $(this),
+			depth1Menu = gnbWrap.find('a'),
+			menuBtn = $('.btnMenu');
+
+		menuBtn.on('click', function(){
+			$('body').toggleClass('menuOn');
+			gnbWrap.slideToggle(300);
+		});
+		depth1Menu.on({
+			'click' : function(){
+				if ($(window).width() < 768) {
+					var $this = $(this),
+						section = $this.attr('href').split('#')[1],
+						top = $('[data-anchor="'+section+'"]').offset().top;
+					$('body').removeClass('menuOn');
+					gnbWrap.slideUp(300);
+					$this.parent().addClass('active').siblings().removeClass('active');
+					$('html, body').animate({scrollTop:top-56},300);
+				}
+			}
+		});
+		$('.header h1 a').on('click', function(){
+			depth1Menu.parent().removeClass('active');
+			if ($(window).width() < 768) {
+				$('html, body').animate({scrollTop:0},300);
+			}
+		});
+	}
+	$('#gnb').gnb();
+});
+var winW = $(window).width(),
+	respChk = "pc";
+function fontSize(w) {
+	if (w <= 560) {
+		var fontSize = w / 5.12;
+		$('html').css('font-size', Math.floor(fontSize*100)/100 + '%');
+	} else {
+		$('html').css('font-size','62.5%');
+	}
+}
+fontSize(winW);
+if (winW < 768 && respChk !== "mo") {
+	respChk = "mo";
+	$(function(){
+		$.fn.fullpage.destroy('all');
+	});
+	$('.mainContent').css('height','calc('+$(window).height()+'px - 5.6rem)');
+} else if (winW > 768 && respChk !== "pc") {
+	respChk = "pc";
+	$('#fullpage').fullpage({
+		menu: '#gnb',
+		navigation: true,
+		responsiveWidth: 780,
+		responsiveHeight: 700
+	});
+}
+$(window).resize(function(){
+	var winW = $(window).width();
+	fontSize(winW);
+	if (winW < 768 && respChk !== "mo") {
+		respChk = "mo";
+		$.fn.fullpage.destroy('all');
+		$('.mainContent').css('height','calc('+$(window).height()+'px - 5.6rem)');
+	} else if (winW > 768 && respChk !== "pc") {
+		respChk = "pc";
+		$('#fullpage').fullpage({
+            menu: '#gnb',
+    		navigation: true,
+            responsiveWidth: 780,
+            responsiveHeight: 700
+        });
 	}
 });
 function getCookie(name) {
