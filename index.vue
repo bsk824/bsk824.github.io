01c1df076dea9a9fdb305c4c7df6260aece52f36
bsk824 <bsk824@gmail.com>
2020-10-03 13:23:53 +0900

    vue test

---
+++ index.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<h1>{{result}}</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			result: 'hello',
+		}
+	},
+	methods: {
+	}
+}
+</script>
+
+<style lang="stylus">
+
+</style>
