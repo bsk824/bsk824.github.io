18d5f9e8f0785e763ce74d7017c530bbea2e0f64
bsk824 <bsk824@gmail.com>
2020-10-15 13:58:57 +0900

    .

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,15 @@ var rename = require('gulp-rename');
 var browserSync = require('browser-sync').create();
 
 var scssOptions = {
-	outputStyle : 'compressed', /* nested, expanded, compact, compressed */
+	outputStyle : 'expanded', /* nested, expanded, compact, compressed */
 	indentType : 'tab',
 	indentWidth : 1,
 	souceComments: true
+}
+var cssName = 'style.css'
+var path = {
+	dest : './resource/',
+	src : './src/',
 }
 gulp.task('html-tpl', function(){
 	var manageEnvironment = function(environment) {
@@ -22,30 +27,30 @@ gulp.task('html-tpl', function(){
 		});
 	};
 
-	return gulp.src('./src/html/**/*.html')
+	return gulp.src(path.src+'html/**/*.html')
 		.pipe(nunjucksRender({
 			envOptions: {
 				autoescape: false
 			},
 			manageEnv : manageEnvironment,
-			path: ['./src']
+			path: [path.src]
 		}))
-		.pipe(gulp.dest('./resource/html'))
+		.pipe(gulp.dest(path.dest+'html'))
 		.pipe(browserSync.reload({stream : true}));
 });
 gulp.task('scss:compile', function(){
 	return gulp
-		.src('./src/scss/**/*.scss')
+		.src(path.src+'scss/**/*.scss')
 		.pipe(sourcemaps.init())
 		.pipe(scss(scssOptions).on('error', scss.logError))
-		.pipe(rename('style.min.css'))
+		.pipe(rename(cssName))
 		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest('./resource/css'))
+		.pipe(gulp.dest(path.dest+'css'))
 		.pipe(browserSync.reload({stream : true}));
 });
 gulp.task('js', function(){
 	return gulp
-		.src('./resource/**/*.js')
+		.src(path.dest+'**/*.js')
 		.pipe(browserSync.reload({stream : true}));
 })
 gulp.task('browserSync', function(){
@@ -57,9 +62,9 @@ gulp.task('browserSync', function(){
 	})
 });
 gulp.task('watch', function(){
-	gulp.watch('./src/**/*.html', gulp.series('html-tpl'));
-	gulp.watch('./src/**/*.scss', gulp.series('scss:compile'));
-	gulp.watch('./resource/**/*.js', gulp.series('js'));
+	gulp.watch(path.src+'**/*.html', gulp.series('html-tpl'));
+	gulp.watch(path.src+'**/*.scss', gulp.series('scss:compile'));
+	gulp.watch(path.dest+'**/*.js', gulp.series('js'));
 });
 
 gulp.task('default', gulp.parallel('watch', 'browserSync'));
