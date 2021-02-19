d2228cf641baee8e99ba9675c171463717afeb5b
Baek Seungki <bsk824@gmail.com>
2021-02-19 13:32:54 +0900

    vue 작업 분리

--- vue/webpack.config.js
+++ vue/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = (env, options) => {
 	const config = {
 		devtool: 'source-map',
 		devServer: {
-			host: "192.168.0.5"
+			host: "localhost"
 		},
 		resolve: {
 			extensions: ['.js', '.vue'],
